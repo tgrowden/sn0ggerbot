175397141a7de42cc77f3383f5f619e81c8ca4a1
tgrowden <taylorgrowden@gmail.com>
2016-03-07 17:45:52 -0500

    as this bot will be a second one on the server, remove call/response and output of /help command

--- controllers/call-response.js
+++ controllers/call-response.js
@@ -1,7 +1,5 @@
 module.exports = function(bot, config, prompts) {
   bot.on('message', function(user, userID, channelID, message, rawEvent) {
-    // custom call-response handlers from ../config/prompts.js
-    bot.CallResponse(prompts.swear, user, userID, channelID, message, rawEvent);
-    bot.CallResponse(prompts.table, user, userID, channelID, message, rawEvent);
+    // no call/response for secondary bot
   });
 };
--- controllers/commands.js
+++ controllers/commands.js
@@ -4,25 +4,13 @@ module.exports = function(bot, config, prompts) {
     var msgArr = message.split(' ');
     // Check if command is coming from bot owner
     if (bot.isOwner(userID, rawEvent)) {
-      if (msgArr[0] == '/help'){
-        var owner_keys = _.keys(bot.ownerCommands);
-        bot.sendMessage({
-          to: channelID,
-          message: "Owner Commands: \n" + owner_keys.join("\n")
-        });
-      } else if (bot.ownerCommands.hasOwnProperty(msgArr[0])) {
+      if (bot.ownerCommands.hasOwnProperty(msgArr[0])) {
         bot.ownerCommands[msgArr[0]].action(user, userID, channelID, message, rawEvent);
       }
     }
     // Check if command is coming from bot admin
     if (bot.isAdmin(userID, rawEvent)) {
-      if (msgArr[0] == '/help'){
-        var keys = _.keys(bot.commands);
-        bot.sendMessage({
-          to: channelID,
-          message: "Commands: \n" + keys.join("\n")
-        });
-      } else if (bot.commands.hasOwnProperty(msgArr[0])) {
+      if (bot.commands.hasOwnProperty(msgArr[0])) {
         bot.commands[msgArr[0]].action(user, userID, channelID, message, rawEvent);
       }else if (bot.ownerCommands.hasOwnProperty(msgArr[0]) && !bot.isOwner(userID)) {
         bot.sendMessage({
@@ -31,7 +19,7 @@ module.exports = function(bot, config, prompts) {
         });
       }
     } else {
-      if (msgArr[0] == '/help' || bot.commands.hasOwnProperty(msgArr[0]) || bot.ownerCommands.hasOwnProperty(msgArr[0])) {
+      if (bot.commands.hasOwnProperty(msgArr[0]) || bot.ownerCommands.hasOwnProperty(msgArr[0])) {
         bot.sendMessage({
           to: channelID,
           message: "I'm sorry, Dave, I'm afraid I can't do that."
