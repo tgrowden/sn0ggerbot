410db27bcdedb17a67e2dcba608f022279a24696
tgrowden <taylorgrowden@gmail.com>
2016-03-07 17:36:19 -0500

    add "grunt audio" command to handle downloading of files, rather than keeping them in the repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    mkdir: {
+      all: {
+        options: {
+          mode: 0755,
+          create: ['audio']
+        }
+      }
+    },
+    curl: {
+      'audio/wuba.mp3': 'http://peal.io/download/i9ixe',
+      'audio/balls.mp3': 'http://peal.io/download/vowb2',
+      'audio/ants.mp3': 'http://peal.io/download/4ate4',
+      'audio/butthole.mp3': 'http://peal.io/download/esmql',
+      'audio/gross.mp3': 'http://peal.io/download/s8w8y',
+      'audio/schwifty.mp3': 'http://peal.io/download/vmvf3',
+      'audio/showme.mp3': 'http://peal.io/download/ah392',
+      'audio/rekt.mp3': 'http://peal.io/download/3tcbx',
+      'audio/booty.mp3': 'http://bluesnogbox.duckdns.org/audio/booty.mp3',
+      'audio/cena.mp3': 'http://bluesnogbox.duckdns.org/audio/cena.mp3',
+      'audio/yeah.mp3': 'http://bluesnogbox.duckdns.org/audio/yeah.mp3'
+    },
     copy: {
       config: {
         files: [{
@@ -11,7 +32,7 @@ module.exports = function(grunt) {
             'config-example.js'
           ],
           rename: function(dest, src) {
-            return dest + src.replace('-example.js','.js');
+            return dest + src.replace('-example.js', '.js');
           }
         }]
       },
@@ -25,7 +46,7 @@ module.exports = function(grunt) {
             'Channels-example.json', 'Roles-example.json', 'Users-example.json', 'Owner-example.json'
           ],
           rename: function(dest, src) {
-            return dest + src.replace('-example.json','.json');
+            return dest + src.replace('-example.json', '.json');
           }
         }]
       },
@@ -39,12 +60,15 @@ module.exports = function(grunt) {
             'admins-example.json'
           ],
           rename: function(dest, src) {
-            return dest + src.replace('-example.json','.json');
+            return dest + src.replace('-example.json', '.json');
           }
         }]
       }
     }
   });
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-curl');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.registerTask('default', ['copy:config', 'copy:info', 'copy:admin']);
+  grunt.registerTask('audio', ['mkdir', 'curl']);
 };
