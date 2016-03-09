f4c22844f6026802b63a136d0fc1a606902c22a6
tgrowden <taylorgrowden@gmail.com>
2016-03-09 16:21:05 -0500

    add option to announce tts messages when user joins/leaves server

--- config/config-example.js
+++ config/config-example.js
@@ -4,6 +4,6 @@ module.exports = {
   channel: "",
   server: '',
   voiceChannel: '',
-  botAdminRole: '',
+  ttsAlerts: false, //set true if you want the bot to send a TTS message when a user leaves/joins server
   debug: false
 };
